77aaad6def974a464e2721358b685f9664a4443c
Adam Samko <samko@alfabase.sk>
2022-05-02 16:27:38 +0200

    pageRank sample implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+
+import Paths_stack_project (getDataFileName)
+import PageRank (process)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "How many iters?"
+  numIters <- getLine
+  fName <- getDataFileName "files/input.txt"
+  f <- readFile fName
+  -- damping factor defaults to 0.85
+  writeFile "output.txt" $ show $ process f (read numIters :: Int) 0.85
