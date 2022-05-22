ab4e3f9e8f163e9cbf1573d85ab0ee70ebf77086
Adam Samko <samko@alfabase.sk>
2022-05-22 14:51:58 +0200

    Some updates

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 
-
+import PageRank (createEdges, process)
 import Paths_stack_project (getDataFileName)
-import PageRank (process)
 
 main :: IO ()
 main = do
-  putStrLn "How many iters?"
-  numIters <- getLine
   fName <- getDataFileName "files/input.txt"
   f <- readFile fName
+  forwardName <- getDataFileName "files/forward2.txt"
+  forward <- readFile forwardName
   -- damping factor defaults to 0.85
-  writeFile "output.txt" $ show $ process f (read numIters :: Int) 0.85
+  writeFile "output2.txt" $ show createEdges
+  writeFile "output.txt" $ show $ process f 3 0.85
--- src/PageRank.hs
+++ src/PageRank.hs
@@ -1,8 +1,14 @@
+{-# HLINT ignore "Use tuple-section" #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module PageRank
   ( process,
+    createEdges,
   )
 where
 
+import Data.List (elemIndex)
 import Data.Map
   ( Map,
     empty,
@@ -15,37 +21,52 @@ import Data.Map
   )
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
+import GHC.Base (Any, VecElem (Int16ElemRep))
 import Paths_stack_project (getDataFileName)
 import Text.Printf (printf)
+import Text.Read (readMaybe)
 import Prelude hiding (lookup)
 
-type Node = Int
+type PageId = Int
 
-type PRValue = Double
+type Rank = Double
 
-type PageRank = Map Node PRValue
+type PageRank = Map PageId Rank
 
-type InboundEdges = Map Node [Node]
+type Edge = (PageId, PageId)
 
-type OutboundEdges = InboundEdges
+type InLinks = Map PageId [PageId]
 
-parseLine :: (InboundEdges, OutboundEdges, Node) -> String -> (InboundEdges, OutboundEdges, Node)
+type OutLinks = InLinks
+
+parseLine :: (InLinks, OutLinks, PageId) -> String -> (InLinks, OutLinks, PageId)
 parseLine (iEdges, oEdges, maxNode) line =
   let ws = words line
-      (from, to) = (read $ ws !! 0, read $ ws !! 1)
+      (from, to) = (read $ head ws, read $ ws !! 1)
    in ( insertWith plusNode to [from] iEdges,
         insertWith plusNode from [to] oEdges,
         max to (max maxNode from)
       )
   where
-    plusNode :: [Node] -> [Node] -> [Node]
+    plusNode :: [PageId] -> [PageId] -> [PageId]
+    plusNode new_node old_node =
+      new_node ++ old_node
+
+parseEdges :: (InLinks, OutLinks, PageId) -> (Int, Int) -> (InLinks, OutLinks, PageId)
+parseEdges (iEdges, oEdges, maxNode) line =
+  ( insertWith plusNode (snd line) [fst line] iEdges,
+    insertWith plusNode (fst line) [snd line] oEdges,
+    max (snd line) (max maxNode (fst line))
+  )
+  where
+    plusNode :: [PageId] -> [PageId] -> [PageId]
     plusNode new_node old_node =
       new_node ++ old_node
 
 newPageRank :: Int -> PageRank
 newPageRank n =
   let v :: Double
-      v = 1 / (fromIntegral n)
+      v = 1 / fromIntegral n
    in go n v empty
   where
     go :: Int -> Double -> PageRank -> PageRank
@@ -56,13 +77,13 @@ newPageRank n =
 -- The goal of postProcess is to deal with the nodes that have no outbound
 -- edges, in which case they should be treated like they have outbound edges
 -- to every other node.
-postProcess :: (InboundEdges, OutboundEdges, Node) -> (InboundEdges, OutboundEdges)
+postProcess :: (InLinks, OutLinks, PageId) -> (InLinks, OutLinks)
 postProcess (iEdges, oEdges, maxNode) =
   let numNodes = maxNode + 1
       newIEdges = addAllNodes (numNodes - 1) iEdges
    in loop (numNodes - 1) newIEdges oEdges
   where
-    loop :: Int -> InboundEdges -> OutboundEdges -> (InboundEdges, OutboundEdges)
+    loop :: Int -> InLinks -> OutLinks -> (InLinks, OutLinks)
     loop n iEdges oEdges
       | n < 0 = (iEdges, oEdges)
       | otherwise =
@@ -74,24 +95,33 @@ postProcess (iEdges, oEdges, maxNode) =
                 newIEdges = mapWithKey (\k v -> if k /= n then v ++ [n] else v) iEdges
              in loop (n - 1) newIEdges newOEdges
 
-    -- This function makes sure that every node is a key in the InboundEdges map
-    addAllNodes :: Int -> InboundEdges -> InboundEdges
+    -- This function makes sure that every node is a key in the InLinks map
+    addAllNodes :: Int -> InLinks -> InLinks
     addAllNodes n iEdges
       | n < 0 = iEdges
       | otherwise =
         addAllNodes (n - 1) $ insertWith (\new old -> new ++ old) n [] iEdges
 
-parseGraph :: String -> (InboundEdges, OutboundEdges, PageRank)
+parseGraph :: String -> (InLinks, OutLinks, PageRank)
 parseGraph input =
   let ls = lines input
       (iEdges, oEdges) = postProcess $ foldl parseLine (empty, empty, 0) ls
       numNodes = size iEdges
    in (iEdges, oEdges, newPageRank numNodes)
 
-loopProcess :: Int -> Double -> InboundEdges -> OutboundEdges -> PageRank -> PageRank
+createEdges :: [Edge]
+createEdges =
+  let outLinks = [[2, 3], [5, 6]] :: [[Int]]
+   in -- headd line = read (head $ words line) :: Int
+      -- taill line = f $ tail line
+      edgeList outLinks
+
+edgeList outLinks = zipWith (\as i -> as >>= pure . (,) i) outLinks [0 ..] >>= id
+
+loopProcess :: Int -> Double -> InLinks -> OutLinks -> PageRank -> PageRank
 loopProcess 0 _ _ _ pageRank = pageRank
 loopProcess n dampingFactor iEdges oEdges pageRank =
-  let newPageRank = loop' ((size pageRank) - 1) empty
+  let newPageRank = loop' (size pageRank - 1) empty
    in loopProcess (n - 1) dampingFactor iEdges oEdges newPageRank
   where
     loop' :: Int -> PageRank -> PageRank
@@ -100,17 +130,19 @@ loopProcess n dampingFactor iEdges oEdges pageRank =
       | otherwise =
         let inbounds = fromJust $ lookup n iEdges
             newPrValue =
-              (+)
-                ((1 - dampingFactor) / (fromIntegral $ size iEdges))
-                (dampingFactor * (foldl calc 0 inbounds))
+              ((1 - dampingFactor) / fromIntegral (size iEdges))
+                + (dampingFactor * foldl calc 0 inbounds)
          in loop' (n - 1) $ insert n newPrValue pr
       where
         calc acc node =
           let outbounds = fromJust $ lookup node oEdges
               prValue = fromJust $ lookup node pageRank
-           in acc + prValue / (fromIntegral $ length outbounds)
+           in acc + prValue / fromIntegral (length outbounds)
 
 process :: String -> Int -> Double -> PageRank
 process input numIters dampingFactor =
   let (iEdges, oEdges, pageRank) = parseGraph input
    in loopProcess numIters dampingFactor iEdges oEdges pageRank
+
+-- vkladam iEdges, OEdges, pageRank ako do loopProcess, ktore sme
+-- dostali z funkcie parseGraph
