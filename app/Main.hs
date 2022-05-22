9ad10f0ee014ff4e56b2e407480373d4cae95591
Adam Samko <samko@alfabase.sk>
2022-05-22 17:38:29 +0200

    added url parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,13 @@
 module Main where
 
-import PageRank (createEdges, process)
+-- import PageRank (process)
+import PageRank (createEdges, position, process')
 import Paths_stack_project (getDataFileName)
 
 main :: IO ()
 main = do
-  fName <- getDataFileName "files/input.txt"
-  f <- readFile fName
-  forwardName <- getDataFileName "files/forward2.txt"
-  forward <- readFile forwardName
-  -- damping factor defaults to 0.85
-  writeFile "output2.txt" $ show createEdges
-  writeFile "output.txt" $ show $ process f 3 0.85
+  let allLinks = [["bing", "lol"], ["google", "lol"], ["google"]]
+  let urlList = ["google", "bing", "lol"]
+
+  writeFile "output2.txt" $ show $ createEdges (map (map (`position` urlList)) allLinks)
+  writeFile "output.txt" $ show $ process' (map (map (`position` urlList)) allLinks) 3 0.85
